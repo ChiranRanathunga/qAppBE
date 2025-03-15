096ca22ebb3063c1a3131072209bd2bd30d4ac8c
Amila de silva <57836010+amila01@users.noreply.github.com>
2025-03-15 12:53:06 +0530

    initial structure

---
+++ src/main/java/com/java/firebase/CRUDRunner.java
@@ -0,0 +1,11 @@
+package com.java.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRUDRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(CRUDRunner.class, args);
+    }
+}
---
+++ src/test/java/com/example/cdlaSaloon/CdlaSaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdlaSaloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdlaSaloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
